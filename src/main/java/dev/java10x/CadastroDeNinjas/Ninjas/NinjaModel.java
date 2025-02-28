154699c4fee0852bcbc9e2f13ec13779c7c005e2
Marmo <marmo.lucas@outlook.com>
2025-02-27 23:28:43 -0300

    Modelagem do banco de dados através das entidades NinjaModel e MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,8 +1,10 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.List;
 
 @Entity
 @Table(name="tb_cadastro_de_ninjas")
@@ -14,6 +16,12 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+
+    //Um ninja só pode atuar em uma única missão.
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreing Key ou Chave Estrangeira
+    private MissoesModel missoes;
+
 
     public NinjaModel() {
     }
