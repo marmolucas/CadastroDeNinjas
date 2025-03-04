0c8f0ae58b3c5481f629299a46c7bfabbad58faf
Marmo <marmo.lucas@outlook.com>
2025-03-04 13:51:11 -0300

    Feat: Coloca o lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,11 +3,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data //Adiciona Getter e Setters
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
