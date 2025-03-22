d4089e28318d82c74054fcfe5cc2d46dc9ef2a7f
Marmo <marmo.lucas@outlook.com>
2025-03-22 18:19:03 -0300

    feat: Criar endpoints para controller Missao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissaoController.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeNinjas.Missoes.Controller;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissaoController {
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao Criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao Alterada com sucesso";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "As missões!";
+    }
+
+    @DeleteMapping("/deletaId")
+    public String deletaMissaoId(){
+        return "Missao deletada com sucesso";
+    }
+
+
+
 }
