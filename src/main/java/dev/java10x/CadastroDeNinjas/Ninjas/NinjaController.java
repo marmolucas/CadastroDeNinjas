9c8d7a0edd0b02d7677a86556f418cea0a191f21
Lucas de Marmo Souza <marmo.lucas@outlook.com>
2025-04-08 21:14:22 -0300

    Ajustes efetuados e inclusão das rotas no controllet

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,9 +3,19 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
@@ -19,8 +29,8 @@ public class NinjaController {
     }
     // Mostrar todos os ninjas (read)
     @GetMapping("/todos")
-    public String mostrarNinjas(){
-        return "Todos os Ninjas";
+    public List<NinjaModel> mostrarNinjas(){
+        return ninjaService.listarNinjas();
     }
 
     // Mostrar Ninja por id (read)
